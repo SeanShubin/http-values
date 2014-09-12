1690f070b08a88312429f56c63c0b54afbdca498
Sean Shubin <seanshubin@gmail.com>
2014-09-12 10:46:34 -0700

    Added integration test

---
+++ jetty_server/src/test/scala/com/seanshubin/http/jetty_server/HttpTest.scala
@@ -0,0 +1,44 @@
+package com.seanshubin.http.jetty_server
+
+import com.seanshubin.http.apache_client.HttpSender
+import com.seanshubin.http.values.{Sender, Receiver, ResponseValue, RequestValue}
+import org.eclipse.jetty.server.Server
+import org.scalatest.FunSuite
+
+class HttpTest extends FunSuite {
+  test("http request") {
+    val sender: Sender = new HttpSender
+    var requests: Seq[RequestValue] = Seq()
+    val expectedResponse = ResponseValue(200, "Hello, world!".getBytes("utf-8"), Map("Content-Type" -> "text/plain; charset=utf-8"))
+    val receiver = new Receiver {
+      override def receive(request: RequestValue): ResponseValue = {
+        requests = requests :+ request
+        expectedResponse
+      }
+    }
+    val port = new FreePortFinderImpl().findFreePort()
+    val server = new Server(port)
+    val handler = new ReceiverToJettyHandler(receiver)
+    server.setHandler(handler)
+    server.start()
+    val sentRequest: RequestValue = RequestValue(s"http://localhost:$port/greeting", "get", Seq(), Map())
+    val expectedRequest: RequestValue = RequestValue(
+      s"/greeting",
+      "GET",
+      Seq(),
+      Map("Connection" -> "keep-alive",
+        "User-Agent" -> "Apache-HttpClient/4.3.5 (java 1.5)",
+        "Host" -> s"localhost:$port",
+        "Accept-Encoding" -> "gzip,deflate"
+      ))
+    val actualResponse = sender.send(sentRequest)
+    server.stop()
+    assert(requests.size === 1)
+    assert(requests(0) === expectedRequest)
+    assert(actualResponse.body === "Hello, world!".getBytes("utf-8").toSeq)
+    assert(actualResponse.statusCode === 200)
+    assert(actualResponse.headers("Content-Type") === "text/plain; charset=UTF-8")
+    assert(actualResponse.headers("Content-Length") === "13")
+    assert(actualResponse.headers("Server") === "Jetty(9.2.2.v20140723)")
+  }
+}
