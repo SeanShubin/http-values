d06f6439d819864a9261d52789b46476d1d6482f
SeanShubin <seanshubin@gmail.com>
2015-11-09 09:23:35 -0800

    Added test for forward slashes in strings

--- core/src/test/scala/com/seanshubin/http/values/core/UriTest.scala
+++ core/src/test/scala/com/seanshubin/http/values/core/UriTest.scala
@@ -21,6 +21,22 @@ class UriTest extends FunSuite {
     assert(uri.toASCIIString === "foo://user@example.com:8042/over/there?name=ferret#nose")
   }
 
+  test("forward slashes in query string") {
+    //see http://tools.ietf.org/html/rfc3986
+    val uri: URI = new URI("foo://user@example.com:8042/over/there?name=/a/b/c#nose")
+    assert(uri.getScheme === "foo")
+    assert(uri.getAuthority === "user@example.com:8042")
+    assert(uri.getPath === "/over/there")
+    assert(uri.getQuery === "name=/a/b/c")
+    assert(uri.getFragment === "nose")
+    assert(uri.getHost === "example.com")
+    assert(uri.getPort === 8042)
+    assert(uri.getSchemeSpecificPart === "//user@example.com:8042/over/there?name=/a/b/c")
+    assert(uri.getUserInfo === "user")
+    assert(uri.toString === "foo://user@example.com:8042/over/there?name=/a/b/c#nose")
+    assert(uri.toASCIIString === "foo://user@example.com:8042/over/there?name=/a/b/c#nose")
+  }
+
   test("compose uri from parts") {
     val uri: URI = new URI("foo", "user", "example.com", 8042, "/over/there", "name=ferret", "nose")
     assert(uri.getScheme === "foo")
