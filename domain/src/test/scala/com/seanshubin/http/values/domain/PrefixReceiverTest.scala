d93bcec97932ddb925a50f87df14b7ab16a60d3c
Sean Shubin <sshubin@cj.com>
2017-04-21 14:23:03 -0700

    removed mock from PrefixReceiverTest

--- domain/src/test/scala/com/seanshubin/http/values/domain/PrefixReceiverTest.scala
+++ domain/src/test/scala/com/seanshubin/http/values/domain/PrefixReceiverTest.scala
@@ -1,75 +1,91 @@
 package com.seanshubin.http.values.domain
 
 import org.scalatest.FunSuite
-import org.scalatest.easymock.EasyMockSugar
 
-class PrefixReceiverTest extends FunSuite with EasyMockSugar {
+import scala.collection.mutable.ArrayBuffer
+
+class PrefixReceiverTest extends FunSuite {
   test("forward along without prefix") {
-    val forwardTo = mock[Receiver]
-    val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
-    val request = RequestValue("http://www.prefix-receiver.test/foo/bar/index.html", "get", Seq(), Seq())
+    // given
+    val originalRequest = RequestValue("http://www.prefix-receiver.test/foo/bar/index.html", "get", Seq(), Seq())
     val modifiedRequest = RequestValue("http://www.prefix-receiver.test/bar/index.html", "get", Seq(), Seq())
-    val response = ResponseValue(200, Seq(), Seq())
-    expecting {
-      forwardTo.receive(modifiedRequest).andReturn(response)
-    }
-    whenExecuting(forwardTo) {
-      prefixReceiver.receive(request)
-    }
+    val originalResponse = ResponseValue(200, Seq(), Seq())
+    val forwardTo = new ReceiverStub(modifiedRequest -> originalResponse)
+    val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
+
+    // when
+    val actualResponse = prefixReceiver.receive(originalRequest)
+
+    // then
+    assert(actualResponse === originalResponse)
+    assert(forwardTo.invocations === Seq(modifiedRequest))
   }
 
   test("rewrite location header") {
-    val forwardTo = mock[Receiver]
-    val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
-    val request = RequestValue("http://www.prefix-receiver.test/foo/bar/index.html", "get", Seq(), Seq())
+    // given
+    val originalRequest = RequestValue("http://www.prefix-receiver.test/foo/bar/index.html", "get", Seq(), Seq())
     val modifiedRequest = RequestValue("http://www.prefix-receiver.test/bar/index.html", "get", Seq(), Seq())
-    val response = ResponseValue(200, Seq(), Seq("location" -> "http://www.prefix-receiver.test/bar/error-page.html"))
+    val originalResponse = ResponseValue(200, Seq(), Seq("location" -> "http://www.prefix-receiver.test/bar/error-page.html"))
+    val forwardTo = new ReceiverStub(modifiedRequest -> originalResponse)
+    val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
     val expectedResponse = ResponseValue(200, Seq(), Seq("location" -> "http://www.prefix-receiver.test/foo/bar/error-page.html"))
-    expecting {
-      forwardTo.receive(modifiedRequest).andReturn(response)
-    }
-    whenExecuting(forwardTo) {
-      val modifiedResponse = prefixReceiver.receive(request)
-      assert(modifiedResponse === expectedResponse)
-    }
+
+    // when
+    val actualResponse = prefixReceiver.receive(originalRequest)
+
+    // then
+    assert(actualResponse === expectedResponse)
+    assert(forwardTo.invocations === Seq(modifiedRequest))
   }
 
   test("handle missing '/' at end") {
-    val forwardTo = mock[Receiver]
-    val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
-    val request = RequestValue("http://www.prefix-receiver.test/foo", "get", Seq(), Seq())
+    // given
     val modifiedRequest = RequestValue("http://www.prefix-receiver.test/", "get", Seq(), Seq())
-    val response = ResponseValue(200, Seq(), Seq())
-    expecting {
-      forwardTo.receive(modifiedRequest).andReturn(response)
-    }
-    whenExecuting(forwardTo) {
-      prefixReceiver.receive(request)
-    }
+    val originalResponse = ResponseValue(200, Seq(), Seq())
+    val forwardTo = new ReceiverStub(modifiedRequest -> originalResponse)
+    val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
+    val originalRequest = RequestValue("http://www.prefix-receiver.test/foo", "get", Seq(), Seq())
+
+    // when
+    val actualResponse = prefixReceiver.receive(originalRequest)
+
+    // then
+    assert(actualResponse === originalResponse)
+    assert(forwardTo.invocations === Seq(modifiedRequest))
   }
 
   test("don't rewrite favicon") {
-    val forwardTo = mock[Receiver]
+    // given
+    val originalRequest = RequestValue("http://www.prefix-receiver.test/favicon.ico", "get", Seq(), Seq())
+    val originalResponse = ResponseValue(200, Seq(), Seq())
+    val forwardTo = new ReceiverStub(originalRequest -> originalResponse)
     val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
-    val request = RequestValue("http://www.prefix-receiver.test/favicon.ico", "get", Seq(), Seq())
-    val response = ResponseValue(200, Seq(), Seq())
-    expecting {
-      forwardTo.receive(request).andReturn(response)
-    }
-    whenExecuting(forwardTo) {
-      prefixReceiver.receive(request)
-    }
+
+    // when
+    val actualResponse = prefixReceiver.receive(originalRequest)
+
+    // then
+    assert(actualResponse === originalResponse)
+    assert(forwardTo.invocations === Seq(originalRequest))
   }
 
   test("fail if prefix does not match") {
-    val forwardTo = mock[Receiver]
+    val forwardTo = new ReceiverStub()
     val prefixReceiver = new PrefixReceiver("/foo", forwardTo)
     val request = RequestValue("http://www.prefix-receiver.test/foobar", "get", Seq(), Seq())
-    whenExecuting(forwardTo) {
-      val exception = intercept[RuntimeException] {
-        prefixReceiver.receive(request)
-      }
-      assert(exception.getMessage === "Expected uri to start with '/foo', got '/foobar'")
+    val exception = intercept[RuntimeException] {
+      prefixReceiver.receive(request)
+    }
+    assert(exception.getMessage === "Expected uri to start with '/foo', got '/foobar'")
+  }
+
+  class ReceiverStub(results: (RequestValue, ResponseValue)*) extends Receiver {
+    val invocations = new ArrayBuffer[RequestValue]()
+
+    override def receive(request: RequestValue): ResponseValue = {
+      invocations.append(request)
+      results.toSeq.toMap.apply(request)
     }
   }
+
 }
