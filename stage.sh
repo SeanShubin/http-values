f4f6d2d15bfd8f7dd79388c2e4640f97e4524647
Sean Shubin <sshubin@cj.com>
2018-01-05 10:31:18 -0800

    Generated project files

--- stage.sh
+++ stage.sh
@@ -4,7 +4,7 @@
 set -e
 
 # make sure we don't inherit any state from our local repository
-rm -rf ~/.m2/repository/com/seanshubin/http/
+rm -rf ~/.m2/repository/com/seanshubin/http/values/
 
 # make sure we don't inherit any state from previous runs
 mvn clean
